dfa066950528caea3e442e0394600094eaef052d
Dennis Riis <dr@driis.dk>
2015-03-07 12:30:00 +0000

    Add empty ASP .NET 5 Console project.

---
+++ src/CS6Examples/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CS6Examples
+{
+    public class Program
+    {
+        public void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.ReadLine();
+        }
+    }
+}
