559d72f8e95027354b8b54dd0bb01308349dab01
Dennis Riis <dr@driis.dk>
2015-03-14 20:07:55 +0000

    Demo instrastructure.

--- src/CS6Examples/Program.cs
+++ src/CS6Examples/Program.cs
@@ -1,13 +1,56 @@
 ﻿using System;
+using System.Linq;
+using static System.Console;
 
 namespace CS6Examples
 {
     public class Program
     {
-        public void Main(string[] args)
+        public void Main(params string[] args)
         {
-            Console.WriteLine("Hello World");
-            Console.ReadLine();
+            if (args.Length > 0)
+            {
+                RunDemoByName(args.First());
+            }
+            else
+            {
+                string name = PromptForDemoToRun();
+                if (!string.IsNullOrWhiteSpace(name))
+                    Main(name);
+
+            }
+            
         }
+
+        private string PromptForDemoToRun()
+        {
+            WriteLine("What would you like to run ?\n");
+            var options = GetType().Assembly.GetTypes().Where(x => typeof(IDemo).IsAssignableFrom(x) && x != typeof(IDemo))
+                .OrderBy(x => x.Name).Select((x, i) => new { Index = i +1, x.Name, Type = x }).ToArray();
+            foreach(var opt in options)
+            {
+                WriteLine($"{opt.Index}: {opt.Name}");
+            }
+            string choice = ReadLine();
+            int? selectedIndex = Parse.Int(choice) - 1;
+            if (selectedIndex != null && selectedIndex < options.Length)
+                return options[selectedIndex.Value].Name;
+            return null;
+        }
+
+        private void RunDemoByName(string name)
+        {
+            var type = GetType().Assembly.GetTypes().FirstOrDefault(x => typeof(IDemo).IsAssignableFrom(x) && x.Name == name);
+            if (type != null)
+            {
+                IDemo demo = (IDemo)Activator.CreateInstance(type);
+                demo.Run();
+            }
+        }
+    }
+
+    public interface IDemo
+    {
+        void Run();
     }
 }
---
+++ src/CS6Examples/Support.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CS6Examples
+{
+    public static class Support
+    {
+
+    }
+
+    public static class Parse
+    {
+        public static int? Int(string value)
+        {
+            int n;
+            if (!int.TryParse(value, out n))
+                return null;
+            return n;
+        }
+    }
+}
