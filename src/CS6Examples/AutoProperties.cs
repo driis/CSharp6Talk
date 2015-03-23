e8046ec2c2f4506c76f2b2409f02b0fe96639a67
Dennis Riis <dr@driis.dk>
2015-03-23 19:32:09 +0000

    AutoProperties example

---
+++ src/CS6Examples/AutoProperties.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace CS6Examples
+{
+    public class AutoProperties : IDemo
+    {
+        public void Run()
+        {
+            var bar = new Bar("test");
+            Console.WriteLine(bar.X);
+        }
+    }
+
+    public class Bar
+    {
+        // A property can now be auto-implemented with only a getter
+        public string X { get; }
+
+        // A getter-only auto property can have an initializer expression:
+        public int Y { get; } = 42;
+
+        public Bar(string x)
+        {
+            // Constructors (and only constructors) can assign to getter-only autoproperties.
+            X = x;
+        }
+    }
+}
