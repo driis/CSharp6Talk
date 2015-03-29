bd0d760bc62f2d46c57533bf07eeb6e3af4ba97c
Dennis Riis <dr@driis.dk>
2015-03-29 18:25:23 +0000

    Dictionary initializer example.

---
+++ src/CS6Examples/DictionaryInitializer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+using static System.Console;
+
+namespace CS6Examples
+{
+    public class DictionaryInitializer : IDemo
+    {
+        public void Run()
+        {
+            // In previous versions, we already had dictionary initializers:
+            var x = new Dictionary<string, string> {
+                { "foo", "bar" },
+                { "x", "42" }
+            };
+            WriteLine(x["foo"]);
+
+            // C# 6 adds an alternative syntax for this
+            var u = new Dictionary<string, string>
+            {
+                ["foo"] = "bar",
+                ["x"] = "42"
+            };
+            WriteLine(u["foo"]);
+        }
+    }
+}
